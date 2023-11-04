6d3331203a39c3ea2dca89d6f018ded565f981c8
david <david@DESKTOP-M81T9R4>
2023-11-04 10:03:25 -0600

    Agregar archivos de proyecto.

---
+++ RazorPageMovie/Migrations/20231104155348_Rating.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RazorPageMovie.Migrations
+{
+    /// <inheritdoc />
+    public partial class Rating : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Rating",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(5)",
+                oldMaxLength: 5);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Rating",
+                table: "Movie",
+                type: "nvarchar(5)",
+                maxLength: 5,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
---
+++ RazorPageMovie/Models/SeedData.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorPageMovie.Data;
+
+namespace RazorPageMovie.Models
+{
+    public class SeedData
+    {
+        public static void Iniciallize(IServiceProvider serviceProvider)
+        {
+            using (var context = new RazorPageMovieContext1(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<RazorPageMovieContext1>>()))
+            {
+                if (context == null || context.Movie == null) { 
+                    throw new ArgumentNullException("Null RazorPageMovieContext");
+                }
+
+                if (context.Movie.Any()) {
+                    return;
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "Harry Potter",
+                        RealeseDate = DateTime.Parse("1989-06-17"),
+                        Genre = "Action",
+                        Price = 300.20M,
+                        Rating = "G"
+                    },
+
+                    new Movie
+                    {
+                        Title = "American Pie",
+                        RealeseDate = DateTime.Parse("2001-12-19"),
+                        Genre = "Comedy",
+                        Price = 8.9M,
+                        Rating = "R"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Jackas",
+                        RealeseDate = DateTime.Parse("2005-08-20"),
+                        Genre = "Action",
+                        Price = 9.21M,
+                        Rating = "G"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Jackas 2",
+                        RealeseDate = DateTime.Parse("2006-08-01"),
+                        Genre = "Action",
+                        Price = 11.21M,
+                        Rating = "NA"
+                    }
+                    ); 
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
